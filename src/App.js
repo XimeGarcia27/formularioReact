c7271ee20edfbe3a1a0cb51938a73b3eebf0476c
XomenaGarcia27 <ximenagaca.27@gmail.com>
2023-09-25 17:40:18 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Container from '@mui/material/Container';
+import FormSignUp from './components/FormSignUp';
+import Typography from '@mui/material/Typography';
+
+function App() {
+
+  const handleSubmit = ( valores ) => {
+
+  }
+
+  return (
+    <Container component="section" maxWidth="sm">
+      <Typography variant='h3' align='center'>Formulario Registro</Typography>
+      <FormSignUp handleSubmit={handleSubmit}/>
+    </Container>
+  );
+}
+
+export default App;
